f85b63585c3b9895c11a962ef93553e73293f9e2
usamayaseendev <usama.yaseen@devsinc.com>
2023-02-21 14:08:23 +0500

    Update HomeStyles.js
    Removed 2 Magic Numbers

--- src/Styles/HomeStyles.js
+++ src/Styles/HomeStyles.js
@@ -1,29 +1,29 @@
 export const styles = {
   lettuce: {
     height: 28,
-    width: 560 + 22,
-    marginBottom: "1%",
+    width: 582,
+    marginBottom: '1%',
     borderRadius: 20,
-    background: "linear-gradient(#228c1d,#91ce50)",
+    background: 'linear-gradient(#228c1d,#91ce50)'
   },
   bacon: {
     height: 12,
     width: 560,
-    marginBottom: "1%",
-    background: "linear-gradient(#bf3813,#c45e38)",
+    marginBottom: '1%',
+    background: 'linear-gradient(#bf3813,#c45e38)'
   },
   cheese: {
     height: 18,
-    width: 560 + 46,
-    marginBottom: "1%",
+    width: 606,
+    marginBottom: '1%',
     borderRadius: 20,
-    background: "linear-gradient(#f4d004,#d6bb22)",
+    background: 'linear-gradient(#f4d004,#d6bb22)'
   },
   meat: {
     height: 48,
     width: 560,
-    marginBottom: "1%",
+    marginBottom: '1%',
     borderRadius: 20,
-    background: "linear-gradient(#7f3608,#702e05)",
-  },
-};
+    background: 'linear-gradient(#7f3608,#702e05)'
+  }
+}
